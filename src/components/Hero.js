725fa6ac7a956caf66ff839f6e52f037019845fd
Javaughn S <savaughnjmith@gmail.com>
2024-07-16 15:36:18 -0400

    created site pages to populate

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+    // pic of me
+    // carousel of character witness statements next to picture of me 
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hero
